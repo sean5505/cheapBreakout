3e5dc7f25b645bfb21d94ac35ce100caf4445e69
sean5505 <idea40@live.com>
2024-06-20 17:12:43 -0400

    code cleanup

--- src/components/GameStats/AllGameStats.tsx
+++ src/components/GameStats/AllGameStats.tsx
@@ -6,20 +6,21 @@ import { useGameStore } from "../../stateManagement/Store";
 import Difficulty from "./Difficulty";
 
 export default function GameStats() {
-  const isGameOver = useGameStore((state)=> state.isGameOver)
-  const isDifficultySelected = useGameStore((state) => state.isDifficultySelected)
+  const { isGameOver, isDifficultySelected } = useGameStore((state) => ({
+    isGameOver: state.isGameOver,
+    isDifficultySelected: state.isDifficultySelected,
+  }));
+
   return (
     <>
-      <div data-testid="gamestats" className="hidden md:block flex-col gap-4 text-center">
-      {!isGameOver && (
-        <>
-        {isDifficultySelected && <Difficulty/> }
-        <Score />
-        <TotalBlocks />
-        <CurrentLevel />
-        <GameTimer />
-        </>
-      )}
+      <div className={`${isGameOver ? "hidden" : "md:block"} hidden `}>
+        <div className="flex flex-col gap-2">
+          {isDifficultySelected ? <Difficulty /> : null}
+          <Score />
+          <TotalBlocks />
+          <CurrentLevel />
+          <GameTimer />
+        </div>
       </div>
     </>
   );
